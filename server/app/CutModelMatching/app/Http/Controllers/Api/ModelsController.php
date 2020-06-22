cedba7ca91d0a5aceb187054325519a0eaeb9cd8
training20-cmm <go.mongodb.mysql@gmail.com>
2020-06-22 16:16:13 +0900

    Added ModelsController@me action

--- server/app/CutModelMatching/app/Http/Controllers/Api/ModelsController.php
+++ server/app/CutModelMatching/app/Http/Controllers/Api/ModelsController.php
@@ -3,14 +3,30 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CustomRequest;
 use App\Http\Requests\QueryRequest;
 use App\Http\Responses\ModelRegistrationResponse;
+use App\Http\Responses\ModelResponse;
 use App\Model;
 use App\QueryAdapter;
 use App\Services\ModelRegistrationService;
 
 class ModelsController extends Controller
 {
+
+    public function me(CustomRequest $request)
+    {
+        $user = self::user($request->token());
+        $model = Model::where("user_id", $user->id)->get()->first();
+        if (is_null($model)) {
+            return self::badRequest();
+        }
+        $queryAdapter = new QueryAdapter();
+        $model = $queryAdapter->executeWithId(Model::class, $request->all(), $model->id)[0];
+        $modelResponse = new ModelResponse();
+        $modelResponse->constructWith($model);
+        return $modelResponse;
+    }
 
     public function register(QueryRequest $request)
     {
